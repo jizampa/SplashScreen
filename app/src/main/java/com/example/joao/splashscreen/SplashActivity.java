650da56b002767d7a40a403ef2a20e814c198ace
jizampa <jizampa@gmail.com>
2018-10-24 10:42:11 -0500

    Changed the app to  use Assync

--- app/src/main/java/com/example/joao/splashscreen/SplashActivity.java
+++ app/src/main/java/com/example/joao/splashscreen/SplashActivity.java
@@ -1,10 +1,11 @@
 package com.example.joao.splashscreen;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,7 +13,7 @@ import android.widget.ImageView;
 public class SplashActivity extends AppCompatActivity {
 
     private ImageView logo;
-    private static int splashTimeOut=5000;
+    private Animation myanim;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,16 +21,34 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         logo=findViewById(R.id.logo);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(i);
-                finish();
-            }
-        },splashTimeOut);
 
-        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
         logo.startAnimation(myanim);
+        RssTask newRss = new RssTask();
+        newRss.execute();
+    }
+
+    class RssTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            Intent i = new Intent(SplashActivity.this,MainActivity.class);
+              startActivity(i);
+              finish();
+        }
     }
 }
